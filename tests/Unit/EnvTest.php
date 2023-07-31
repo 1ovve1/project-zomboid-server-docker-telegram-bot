6f4e32176d8b6af33e2c69312cba58cf4f1331d9
Andrey Bulavskiy <10vvvvvv31@gmail.com>
2023-07-31 15:20:43 +0300

    test: :white_check_mark: Unit Tests For Env

---
+++ tests/Unit/EnvTest.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace PZBot\Tests\Unit;
+use PHPUnit\Framework\TestCase;
+use PZBot\Env;
+use PZBot\Exceptions\Unchecked\EnvParameterNotFoundException;
+
+class EnvTest extends TestCase
+{
+  const ENV_VALUES = [
+    "TEST" => 123456,
+    "TEST_COMPLEX" => [
+      "BRANCH1" => 123,
+      "BRANCH2" => [
+        "BRANCH3" => 456,
+      ],
+    ]
+  ];
+  public Env $config;
+
+  function setUp(): void
+  {
+    $_ENV = self::ENV_VALUES;
+    $this->config = new Env();
+
+    parent::setUp();
+  }
+
+  function testSimpleParam(): void
+  {
+    $param = $this->config->get("TEST");
+
+    $this->assertEquals(self::ENV_VALUES["TEST"], $param);
+  }
+
+  function testComplexParam(): void
+  {
+    $param = $this->config->get("TEST_COMPLEX.BRANCH1");
+
+    $this->assertEquals(self::ENV_VALUES["TEST_COMPLEX"]["BRANCH1"], $param);
+  }
+
+  function testException(): void
+  {
+    $this->expectException(EnvParameterNotFoundException::class);
+
+    $this->config->get("TEST_COMPLEX.BRANCH3");
+  }
+}
